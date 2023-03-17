226629c97cc9b94aa2d0b7846f5957367e8258aa
marksweeneyster <marksweeneyster@gmail.com>
2023-03-16 19:53:30 -0600

    more fmt, and more cmake cleanup

--- main.cpp
+++ main.cpp
@@ -1,31 +1,45 @@
-#include <format>
-#include <iostream>
 #include "FuInterface.hpp"
-
 #include "fmt/format.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+template <> struct fmt::formatter<fubar::FuErr> {
+    // boilerplate for this simple case
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        return ctx.begin();
+    }
 
+    template <typename FormatContext>
+    auto format(const fubar::FuErr& err, FormatContext& ctx) const -> decltype(ctx.out()) {
+        // ctx.out() is an output iterator to write to.
+        switch(err) {
+            case fubar::FuErr::None:
+                return fmt::format_to(ctx.out(), "None");
+            case fubar::FuErr::Unknown:
+            default:
+                return fmt::format_to(ctx.out(), "Unknown");
+        }
+    }
+};
+
+int main() {
     auto a = 2.14;
     auto b = 3.98;
 
-    std::cout << "a=" << a << ", b=" << b << "\n\n";
+    fmt::print("Fubar \"add\" {0:#} and {1:#}\n\n",a,b);
 
     auto fubar = fubar::FuFactory::MakeFuTypeOne();
 
-    std::cout << std::format(" foo test: {}\n", fubar->add(a,b));
-    std::cout << " foo err: " << fubar->getErr() << "\n\n";
+    fmt::print(" foo test: {0:#}\n", fubar->add(a,b));
+    fmt::print(" foo err: {}\n\n", fubar->getErr());
 
     fubar = fubar::FuFactory::MakeFuTypeTwo();
 
-    std::cout << std::format(" foo test2: {}\n", fubar->add(a,b));
-    std::cout << " foo2 err: " << fubar->getErr() << "\n\n";
+    fmt::print(" foo test2: {0:#}\n", fubar->add(a,b));
+    fmt::print(" foo err: {}\n\n", fubar->getErr());
 
     fubar = fubar::FuFactory::MakeFuNoop();
 
-    std::cout << std::format(" foo test noop: {}\n", fubar->add(a,b));
-    std::cout << " noop err: " << fubar->getErr() << "\n";
+    fmt::print(" foo noop: {0:#}\n", fubar->add(a,b));
+    fmt::print(" foo err: {}\n", fubar->getErr());
 
     return 0;
 }
